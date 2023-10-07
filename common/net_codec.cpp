6e49f188d462ef3c99433d83dc9b483ec6edfbf7
mengyuanyang1515 <mengyuanyang1515@hotmail.com>
2023-10-07 22:12:46 +0800

    修复一个粘包的bug

--- common/net_codec.cpp
+++ common/net_codec.cpp
@@ -40,6 +40,7 @@ void NetCodec::SetMessageCallback(MessageCallback message_callback)
 }
 
 
+// 粘包逻辑  
 void NetCodec::ReadyRead()
 {
     qDebug() << "NetCodec调用ReadyRead3";
@@ -54,9 +55,9 @@ void NetCodec::ReadyRead()
         {
             short len = qFromBigEndian<short>((uchar*)tcp_socket_info.byte_array_.left(kHeaderLen).data());
             qDebug() << "kHeaderLen:" << kHeaderLen << " kMessageIdLen:" << kMessageIdLen << " 粘包 len:" << len;
-            if (tcp_socket_info.byte_array_.size() >= (len - kHeaderLen))
+            if (tcp_socket_info.byte_array_.size() >= (len + kHeaderLen))
             {
-                unsigned int msg_id = qFromBigEndian<unsigned int>((uchar*)tcp_socket_info.byte_array_.mid(kHeaderLen, 4).data());
+                unsigned int msg_id = qFromBigEndian<unsigned int>((uchar*)tcp_socket_info.byte_array_.mid(kHeaderLen, kMessageIdLen).data());
                 QByteArray data = tcp_socket_info.byte_array_.mid(kHeaderLen + kMessageIdLen, len - kMessageIdLen);
                 
                 if (message_callback_ != nullptr)
@@ -64,7 +65,7 @@ void NetCodec::ReadyRead()
                     message_callback_(socket, msg_id, data.constData(), len - kMessageIdLen);
                 }
                 
-                data.remove(0, kHeaderLen + len);
+                tcp_socket_info.byte_array_.remove(0, kHeaderLen + len);
             }
         }
     }
@@ -77,9 +78,9 @@ void NetCodec::WriteMessage(unsigned int message_id, const char *data, int len, 
         return;
     }
     
-    short messageLen_big = qToBigEndian(len + 4);
+    short messageLen_big = qToBigEndian<short>(len + kMessageIdLen);
     socket->write((const char*)(&messageLen_big), sizeof(messageLen_big));
-    unsigned int msg_id_big = qToBigEndian(message_id);
+    unsigned int msg_id_big = qToBigEndian<unsigned int>(message_id);
     socket->write((const char*)(&msg_id_big), sizeof(msg_id_big));
     socket->write(data);
 }
